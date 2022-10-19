ecf15c6155a5dddbaa2ad0d80082f50f8f7a8467
cohurin <neila@users.noreply.github.com>
2022-10-19 16:59:39 +0900

    hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,45 +1,43 @@
-import * as dotenv from "dotenv";
+import * as dotenv from "dotenv"
 
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config"
 import "@openzeppelin/hardhat-upgrades"
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
+import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-waffle"
+import "@typechain/hardhat"
+import "hardhat-gas-reporter"
+import "solidity-coverage"
 
-dotenv.config();
+dotenv.config()
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// Sample Hardhat task. See: https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//     const accounts = await hre.ethers.getSigners()
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+//     for (const account of accounts) {
+//         console.log(account.address)
+//     }
+// })
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
-  networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    solidity: "0.8.17",
+    networks: {
+        ropsten: {
+            url: process.env.ROPSTEN_URL || "",
+            accounts:
+                process.env.PRIVATE_KEY !== undefined
+                    ? [process.env.PRIVATE_KEY]
+                    : [],
+        },
     },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
-};
+    gasReporter: {
+        enabled: process.env.REPORT_GAS !== undefined,
+        currency: "USD",
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+}
 
-export default config;
+export default config
