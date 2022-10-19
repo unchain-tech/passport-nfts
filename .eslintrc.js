456cd8fd2ce89483f68c1fbe40839d5c38ace0bb
cohurin <neila@users.noreply.github.com>
2022-10-19 17:00:33 +0900

    linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,23 @@
 module.exports = {
-  env: {
-    browser: false,
-    es2021: true,
-    mocha: true,
-    node: true,
-  },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  rules: {
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      { ignores: ["modules"] },
-    ],
-  },
-};
+    env: {
+        browser: false,
+        es2021: true,
+        mocha: true,
+        node: true,
+    },
+    plugins: ["@typescript-eslint"],
+    extends: ["standard", "prettier", "plugin:node/recommended"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {
+        "node/no-unsupported-features/es-syntax": [
+            "error",
+            { ignores: ["modules"] },
+        ],
+    },
+    settings: {
+        node: { tryExtensions: [".js", ".json", ".node", ".ts", ".d.ts"] },
+    },
+}
