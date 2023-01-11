984b04a8a570e0ffee95051a18166fb94b58463c
Yuka Saito <yksaito02@gmail.com>
2023-01-11 13:33:24 +0900

    delete unnecessary test-code

--- test/4.controlContract.test.ts
+++ test/4.controlContract.test.ts
@@ -4,8 +4,6 @@ import { ethers, upgrades } from "hardhat"
 import { Contract, ContractFactory } from "ethers"
 
 let controlContract: Contract
-// let textContract1: Contract
-// let textContract2: Contract
 let textContractTest: Contract
 
 describe("Control Contract", function () {
@@ -14,12 +12,6 @@ describe("Control Contract", function () {
         const controlFactory: ContractFactory = await ethers.getContractFactory(
             "ControlContract",
         )
-        // const textFactory1: ContractFactory = await ethers.getContractFactory(
-        //     "TextContractTest_1",
-        // )
-        // const textFactory2: ContractFactory = await ethers.getContractFactory(
-        //     "TextContractTest_2",
-        // )
         const textFactoryTest: ContractFactory =
             await ethers.getContractFactory("TextContract")
 
@@ -29,26 +21,10 @@ describe("Control Contract", function () {
 
         await controlContract.deployed()
 
-        // textContract1 = await upgrades.deployProxy(
-        //     textFactory1,
-        //     [controlContract.address],
-        //     {
-        //         initializer: "initialize",
-        //     },
-        // )
-        // textContract2 = await upgrades.deployProxy(
-        //     textFactory2,
-        //     [controlContract.address],
-        //     {
-        //         initializer: "initialize",
-        //     },
-        // )
         textContractTest = await upgrades.deployProxy(textFactoryTest, [], {
             initializer: "initialize",
         })
 
-        // await textContract1.deployed()
-        // await textContract2.deployed()
         await textContractTest.deployed()
     })
 
@@ -167,48 +143,4 @@ describe("Control Contract", function () {
             console.log(iface.parseLog(txData.events[i]).args)
         }
     })
-
-    // // test
-    // it("get text contract information", async function () {
-    //     const [userA, userB] = await ethers.getSigners()
-    //     await controlContract.addTextContractAddress(textContract1.address)
-    //     await controlContract.addTextContractAddress(textContract2.address)
-    //     const textAddressList =
-    //         await controlContract.showTextContractAddressList()
-
-    //     // // change mint status to UNAVAILABLE
-    //     // await controlContract
-    //     //     .connect(userB)
-    //     //     .changeStatusUnavailable(textContract1.address)
-
-    //     // // change mint status to AVAILABLE
-    //     // await controlContract
-    //     //     .connect(userB)
-    //     //     .changeStatusAvailable(textContract1.address)
-
-    //     // change mint status to DONE(text contract 1)
-    //     await controlContract
-    //         .connect(userA)
-    //         .changeStatusDone(textContract1.address)
-
-    //     // change mint status to DONE(text contract 2)
-    //     await controlContract
-    //         .connect(userB)
-    //         .changeStatusAvailable(textContract2.address)
-
-    //     // get user status
-    //     const txForGetUserStatus = await controlContract
-    //         .connect(userB)
-    //         .getTexts(textAddressList)
-
-    //     // select which event to get
-    //     const abi = ["event getUserStatus((uint256, string, address, uint8)[])"]
-    //     const iface = new ethers.utils.Interface(abi)
-    //     const txData = await txForGetUserStatus.wait()
-
-    //     // decode the all event's output and display
-    //     for (let i = 0; i < txData.events.length; i++) {
-    //         console.log(iface.parseLog(txData.events[i]).args)
-    //     }
-    // })
 })
