6be990b3d7a2b8a105b0563efda945fd4273bc25
github honganji <yujicavs@icloud.com>
2023-03-06 23:29:28 +0900

    add read csv function

---
+++ packages/client/components/molecules/csvReadButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { readCSV } from '../../hooks/uiFunction';
+
+export default function CsvReadButton() {
+  return (
+    <div>
+      <input
+        type={'file'}
+        name={'file'}
+        accept={'.csv'}
+        onChange={readCSV}
+        className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+      />
+    </div>
+  );
+}
